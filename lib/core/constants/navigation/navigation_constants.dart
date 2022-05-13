2196caba37d72fabea1e80a19ec01db3d98b9bcd
ezgi98 <ezgicetinkaya_64@hotmail.com>
2022-05-13 23:25:03 +0300

    product pages ready, naviagationManager added

---
+++ lib/core/constants/navigation/navigation_constants.dart
@@ -0,0 +1,5 @@
+//constant page names will be routed
+mixin NavigationConstants {
+  static const String product_info = '/product_info_page';
+  static const String client_profile = '/client_profile_page';
+}
