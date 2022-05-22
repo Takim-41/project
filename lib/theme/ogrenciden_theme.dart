eb9a6c7f78cbfeaf82f2062a8e627dbbcef971f9
Yusuf Başpınar <y.baspinar@yandex.com>
2022-05-23 00:15:57 +0300

    [Feature] Theme
    Created centralized theme for app

---
+++ lib/theme/ogrenciden_theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OgrencidenTheme {
+  static ThemeData get lightTheme {
+    //1
+    return ThemeData(
+        //2
+        primaryColor: const Color(0xff2b6ea7),
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Montserrat', //3
+        buttonTheme: ButtonThemeData(
+          // 4
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
+          buttonColor: const Color(0xff2b6ea7),
+        ));
+  }
+}
