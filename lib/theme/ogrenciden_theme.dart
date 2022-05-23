3b38d9fdb1f1c20d525aca30851db207640a1124
Yusuf Başpınar <y.baspinar@yandex.com>
2022-05-23 04:28:11 +0300

    [Update] PrimaryColor,FontFamily&ButtonColor

--- lib/theme/ogrenciden_theme.dart
+++ lib/theme/ogrenciden_theme.dart
@@ -5,14 +5,15 @@ class OgrencidenTheme {
     //1
     return ThemeData(
         //2
-        primaryColor: const Color(0xff2b6ea7),
+        primaryColor: Colors.white,
         scaffoldBackgroundColor: Colors.white,
-        fontFamily: 'Montserrat', //3
+        bottomAppBarColor: Colors.white.withOpacity(0.9),
+        fontFamily: 'Poppins', //3
         buttonTheme: ButtonThemeData(
           // 4
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
-          buttonColor: const Color(0xff889bab),
+          buttonColor: const Color(0xff2b6ea7),
         ));
   }
 }
