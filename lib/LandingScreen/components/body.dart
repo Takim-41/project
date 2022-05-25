46648bc410ba82d1793c552f8e628a940dcdadea
ArdaP <yuandarcoop@gmail.com>
2022-05-25 17:54:57 +0300

    This version is built with remaining features: - Logo is created and imply on the top of the screeen. - Search Bar is created but non-functiable. (Not connected to searching method.) - Universities list is created with their Logo. (Logo and names of the unis. are taken from local storage.) - Popular Categories are added on the landing page (Not connected related screens.) - Navigation Bottom Bar created and color is changed with button according to navigation buttons (buttons are not connected the screens.)

---
+++ lib/LandingScreen/components/body.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ogrenciden/LandingScreen/components/search_bar.dart';
+import 'package:ogrenciden/LandingScreen/components/uni_category.dart';
+import 'package:ogrenciden/LandingScreen/components/universities.dart';
+import 'package:ogrenciden/size_config.dart';
+
+import 'app_name.dart';
+import 'categories.dart';
+
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+  static const kSecondaryColor = Color(0xFF979797);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height:100),
+            AppName(),
+            SizedBox(height:30),
+            SearchBar(),
+            SizedBox(height:15),
+            UniCategory(
+              text: "Universities",
+              press: () {},
+            ),
+            SizedBox(height:15),
+            Universities(),
+            SizedBox(height:15),
+            UniCategory(
+              text: "Popular Categories",
+              press: () {}
+            ),
+            SizedBox(height:15),
+            Categories(),
+          ]
+        )
+      ),
+    );
+  }
+}
+
