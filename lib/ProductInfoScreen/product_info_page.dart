6059e6fff197585cf3064dddc66b37b39f244ac1
ArdaP <yuandarcoop@gmail.com>
2022-05-31 20:33:21 +0300

    This version is built with remaining features: - Connect the productList and productScreen are connected eachother.

---
+++ lib/ProductInfoScreen/product_info_page.dart
@@ -0,0 +1,183 @@
+import 'package:badges/badges.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import 'package:ogrenciden/ClientProfileInfo/client_profile_info.dart';
+
+String imagePath = "assets/images/";
+
+final List<String> imgList = ["${imagePath}book1.jpg", "${imagePath}book2.jpg"];
+
+// final List<String> imgList = [
+//   'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+//   'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+//   'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+//   'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+//   'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+//   'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+// ];
+class ProductInfoPage extends StatefulWidget {
+  const ProductInfoPage({Key? key}) : super(key: key);
+  static String routeName = "/ProductInfoScreen";
+
+
+  @override
+  State<ProductInfoPage> createState() => _ProductInfoPageState();
+}
+
+class _ProductInfoPageState extends State<ProductInfoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Book Info'),
+        backgroundColor: Colors.green,
+        leading: const BackButton(
+          color: Colors.black,
+          //onPressed: () {},
+        ),
+        actions: <Widget>[
+          IconButton(
+            iconSize: 30.0,
+            icon: Icon(
+              Icons.shopping_basket_sharp,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // do something
+            },
+          ),
+          // Badge(
+          //   badgeContent: Text('3'),
+          //   child: Icon(Icons.shopping_basket_sharp),
+          // )
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: CarouselSlider(
+              options: CarouselOptions(
+                //autoPlay: true,
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
+              ),
+              items: imageSliders,
+            ),
+          ),
+          Text(
+            "Calculus 1",
+            style: const TextStyle(fontSize: 32.0, color: Colors.grey),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Row(
+                  children: const [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Description",
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    "This book is second hand. It may help your work in CALCULUS I lecture in university. Our teachers offered and really popular book in engineering.",
+                    style: TextStyle(fontSize: 17.0, color: Colors.grey),
+                  ),
+                ),
+                // Align(
+                //   alignment: Alignment.bottomRight,
+                //   child: IconButton(
+                //     icon: Icon(Icons.add),
+                //     tooltip: 'Increase volume by 10',
+                //     onPressed: () {
+                //       print("ss");
+                //     },
+                //   ),
+                // ),
+                Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 2),
+                      color: Colors.green,
+                    ),
+                    child: IconButton(
+                      iconSize: 25,
+                      padding: const EdgeInsets.all(20),
+                      icon: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        // do something here
+                      },
+                    )),
+                ElevatedButton(
+                    onPressed: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new ClientProfileInfo())),
+                    child: Text("Contact Info"))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  final List<Widget> imageSliders = imgList
+      .map((item) => Container(
+    child: Container(
+      margin: EdgeInsets.all(5.0),
+      child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          child: Stack(
+            children: <Widget>[
+              Image.asset(item, fit: BoxFit.cover, width: 1000.0),
+              Positioned(
+                bottom: 0.0,
+                left: 0.0,
+                right: 0.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(200, 0, 0, 0),
+                        Color.fromARGB(0, 0, 0, 0)
+                      ],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                    ),
+                  ),
+                  padding: EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 20.0),
+                  child: Text(
+                    'No. ${imgList.indexOf(item)} image',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )),
+    ),
+  ))
+      .toList();
+}
